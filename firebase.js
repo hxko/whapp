592947c13d78d75b52eff6742c125d1c4fc27f40
hxko <heikosohn@gmail.com>
2025-07-12 16:05:23 +0200

    firebase

--- firebase.js
+++ firebase.js
@@ -1,16 +1,15 @@
 // firebase.js
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAmaRKI89LYuqzBWVkPhpZZV14mx9poSHM",
-  authDomain: "whappsapp-2.firebaseapp.com",
-  projectId: "whappsapp-2",
-  storageBucket: "whappsapp-2.appspot.com", // Corrected storage bucket URL
-  messagingSenderId: "552032052369",
-  appId: "1:552032052369:web:680759f13c299905cf4b30"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
