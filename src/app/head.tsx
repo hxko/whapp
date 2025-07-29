b41f19207fdc26e676575a03d74262f7deca93e8
hxko <heikosohn@gmail.com>
2025-07-30 01:09:21 +0200

    Add head.tsx

---
+++ src/app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="preconnect" href="https://firestore.googleapis.com" />
+      <link rel="preconnect" href="https://googleapis.com" />
+      <link rel="preconnect" href="https://identitytoolkit.googleapis.com" />
+
+      <link rel="dns-prefetch" href="https://firestore.googleapis.com" />
+      <link rel="dns-prefetch" href="https://googleapis.com" />
+      <link rel="dns-prefetch" href="https://identitytoolkit.googleapis.com" />
+    </>
+  );
+}
